0b9fd0a8eadb091707774a7c7a65b9d41d296449
BrenoHenriqueBatista <breno.henriquefacul@gmail.com>
2023-08-18 19:14:51 -0300

    utilizando promises para programacao assincrona

--- index.js
+++ index.js
@@ -5,15 +5,29 @@ function trataErro(erro){
     throw new Error(chalk.red(erro.code, 'nào há arquivo no diretório'));
 }
 
-
 function pegaArquivo(caminhoDoArquivo){
     const encoding = 'utf-8';
-    fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
-        if (erro){
-            trataErro(erro);
-        }
-        console.log(chalk.green(texto));
-    })
+    fs.promises
+    .readFile(caminhoDoArquivo, encoding)
+    .then((texto) => console.log(chalk.green(texto)))
+    .catch(trataErro)
 }
+
+
+
+
+
+
+
+
+// function pegaArquivo(caminhoDoArquivo){
+//     const encoding = 'utf-8';
+//     fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//         if (erro){
+//             trataErro(erro);
+//         }
+//         console.log(chalk.green(texto));
+//     })
+// }
 
 pegaArquivo('./arquivos/texto.md');
